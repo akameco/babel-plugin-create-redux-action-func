1f9c9a56c90e69ac9e442d7258dd5c8f46e516de
akameco <akameco.t@gmail.com>
2017-06-02 07:39:53 +0900

    tweak test

--- test/nest.test.js
+++ test/nest.test.js
@@ -3,21 +3,21 @@ import transformCode from './transform'
 
 test('nest union type', () => {
   const input = `
-    type HOGE_TYPE = 'hoge/HOGE_ACTION'
-    type FOO_TYPE = 'hoge/FOO_ACTION'
-    type Action = { +type: HOGE_TYPE | FOO_TYPE }
+type HOGE_TYPE = 'hoge/HOGE_ACTION'
+type FOO_TYPE = 'hoge/FOO_ACTION'
+type Action = { +type: HOGE_TYPE | FOO_TYPE }
   `
   expect(transformCode(input)).toMatchSnapshot()
 })
 
 test('nest union type with args', () => {
   const input = `
-    type HOGE_TYPE = 'hoge/HOGE_ACTION'
-    type FOO_TYPE = 'hoge/FOO_ACTION'
-    type FOO_FUGA_TYPE = 'hoge/FOO_ACTION'
-    type Action =
-      | { +type: HOGE_TYPE | FOO_TYPE }
-      | { +type: FOO_FUGA_TYPE, id: number }
+type HOGE_TYPE = 'hoge/HOGE_ACTION'
+type FOO_TYPE = 'hoge/FOO_ACTION'
+type FOO_FUGA_TYPE = 'hoge/FOO_ACTION'
+type Action =
+  | { +type: HOGE_TYPE | FOO_TYPE }
+  | { +type: FOO_FUGA_TYPE, id: number }
   `
   expect(transformCode(input)).toMatchSnapshot()
 })
